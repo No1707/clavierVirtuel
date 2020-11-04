1d1d03724e86b30888bcc2b9827707becf3bf415
No1707 <nolanwebpro@gmail.com>
2020-11-04 14:43:31 +0100

    styled

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 
+//time
+const options = { day: 'numeric', month: 'numeric', hour: 'numeric', minute: 'numeric' };
+const elem = document.querySelector("#time")
+
+setInterval( () => {
+    window.time = new Date()
+
+    elem.innerHTML = time.toLocaleDateString("fr-FR", options)
+}, 1000)
+
+
 const toggleButton = document.querySelector(".toggleButton")
 
 toggleButton.addEventListener("click", () => {
