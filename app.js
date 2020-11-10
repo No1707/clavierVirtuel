f072f954ca75bfaa3debe37a573e5257ac6bbd81
No1707 <nolanwebpro@gmail.com>
2020-11-10 13:07:15 +0100

    avancement

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 
 //time
-const options = { day: 'numeric', month: 'numeric', hour: 'numeric', minute: 'numeric' };
-const elem = document.querySelector("#time")
+const options = { day: 'numeric', month: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+const timeDisplay = document.querySelector("#time")
 
 setInterval( () => {
     window.time = new Date()
 
-    elem.innerHTML = time.toLocaleDateString("fr-FR", options)
+    timeDisplay.innerHTML = time.toLocaleDateString("fr-FR", options)
 }, 1000)
 
-
+// night/light mode
 const toggleButton = document.querySelector(".toggleButton")
 
 toggleButton.addEventListener("click", () => {
    
-    const styleSheet = document.getElementsByTagName("link")[6]
+    const styleSheet = document.getElementsByTagName("link")[3]
     if( styleSheet.getAttribute("href") === "keyboard-light.css"){
 
         styleSheet.setAttribute("href", "keyboard-night.css")
@@ -28,3 +28,13 @@ toggleButton.addEventListener("click", () => {
     }
 
 })
+
+
+//keys
+const keys = document.querySelectorAll(".key")
+
+for( i = 0; i<keys.length; i++){
+    keys[i].addEventListener("click", (e) => {
+        console.log(e.target)
+    })
+}
